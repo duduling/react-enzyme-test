7398b84a7c45e5b24397a9463af711b4ffa8e4ae
Duduling <duduling20@gmail.com>
2020-08-28 08:22:45 +0900

    feat Add counter test code

---
+++ src/Counter.test.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import { shallow } from 'enzyme'
+import Counter from './Counter'
+
+describe('<Counter />', () => {
+    it('matches snapshot', () => {
+        const wrapper = shallow(<Counter />)
+        expect(wrapper).toMatchSnapshot()
+    })
+    it('has initial number', () => {
+        const wrapper = shallow(<Counter />)
+        expect(wrapper.state().number).toBe(0)
+    })
+    it('increases', () => {
+        const wrapper = shallow(<Counter />)
+        wrapper.instance().handleIncrease()
+        expect(wrapper.state().number).toBe(1)
+    })
+    it('decreases', () => {
+        const wrapper = shallow(<Counter />)
+        wrapper.instance().handleDecrease()
+        expect(wrapper.state().number).toBe(-1)
+    })
+})
