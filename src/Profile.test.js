9dde53a2d88ff00bac1e8818c0a3d1f0dd3a411b
Duduling <duduling20@gmail.com>
2020-09-01 20:13:26 +0900

    feat: Add test code for Profile.js

--- src/Profile.test.js
+++ src/Profile.test.js
@@ -1,18 +1,17 @@
 import React from 'react'
-import { render, getByText } from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
 
 import Profile from './Profile'
 
 describe('<Profile />', () => {
     it('matches snapshot', () => {
-        const { container } = render(<Profile username="Duduling" name="두들링" />)
+        const { container } = render(<Profile username='Duduling' name='두들링' />)
         expect(container).toMatchSnapshot()
     })
     it('shows ther props correctly', () => {
-        const { container } = render(<Profile usename={'Duduling'} name={'두들링'} />)
-
-        getByText(container, 'Duduling')
-        getByText(container, '(두들링)')
-        getByText(container, /김/)
+        render(<Profile username='Duduling' name='두들링' />)
+        screen.getByText('Duduling')
+        screen.getByText('(두들링)')
+        screen.getByText(/두/)
     })
-})
+}) 
